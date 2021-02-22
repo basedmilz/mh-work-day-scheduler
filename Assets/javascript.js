7a6e3b7c63a747febc5e665a2f68256e935a169d
basedmilz <basedmilz@gmail.com>
2021-02-22 17:47:36 -0500

    updated storage

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -3,3 +3,5 @@ $(document).ready(function () {
 $("#day").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 })
+var plan = $(document).siblings(".col-md-10 description")
+window.localStorage.setItem(plan)
